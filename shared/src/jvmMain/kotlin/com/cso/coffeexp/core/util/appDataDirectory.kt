3573513c5f85e8ac67c074842fc8749f421dffb3
cainan <csoliveira01@gmail.com>
2026-07-27 21:33:40 -0400

    Fixing folder names to desktop database

--- shared/src/jvmMain/kotlin/com/cso/coffeexp/core/util/appDataDirectory.kt
+++ shared/src/jvmMain/kotlin/com/cso/coffeexp/core/util/appDataDirectory.kt
@@ -6,8 +6,8 @@ val appDataDirectory: File
     get() {
         val userHome = System.getProperty("user.home")
         return when (currentOs) {
-            DesktopOs.WINDOWS -> File(System.getenv("APPDATA"), "Chirp")
-            DesktopOs.MACOS -> File(userHome, "Library/Application Support/Chirp")
-            DesktopOs.LINUX -> File(userHome, ".local/share/Chirp")
+            DesktopOs.WINDOWS -> File(System.getenv("APPDATA"), "CoffeeXp")
+            DesktopOs.MACOS -> File(userHome, "Library/Application Support/CoffeeXp")
+            DesktopOs.LINUX -> File(userHome, ".local/share/CoffeeXp")
         }
     }
